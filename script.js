d3691fb61c47c363dbce4f8281e4914e2407e799
Thanh150403 <nguyenhuuthanh150403@gmail.com>
2025-03-12 10:53:29 +0700

    Resolve merge conflict

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-// Hiệu ứng đổi màu tên khi rê chuột vào
-document.getElementById("name").addEventListener("mouseover", function() {
-    this.style.color = "#ffdd57";
-});
-document.getElementById("name").addEventListener("mouseout", function() {
-    this.style.color = "white";
-});
-
-// Thay đổi nội dung giới thiệu khi click vào
-document.getElementById("about-text").addEventListener("click", function() {
-    this.innerText = "Tôi luôn đam mê khám phá và ứng dụng công nghệ vào thực tế!";
-});
-
-// Hiển thị thông báo khi nhấn vào email
-document.getElementById("email").addEventListener("click", function() {
-    alert("Hãy liên hệ với tôi qua email: " + this.innerText);
-});
+// function showCV(index) {
+//     let cvSections = document.querySelectorAll('.cv-content');
+//     cvSections.forEach((cv, i) => {
+//         cv.style.display = (i === index - 1) ? 'block' : 'none';
+//     });
+// }
